e3e8415564d8bb35d664948cea70c70f85cc6b69
AyurDondokov <ayur00dondokov@gmail.com>
2022-11-22 11:18:56 +0800

    4

--- simpeCircleNumbers.py
+++ simpeCircleNumbers.py
@@ -1,25 +1,30 @@
-import itertools
 
 def is_simple(number):
-    if number == 2 or number == 3: return True
-    if number % 2 == 0 or number < 2: return False
+    if number == 2 or number == 3:
+        return True
+    if number % 2 == 0 or number < 2:
+        return False
     for i in range(3, int(number ** 0.5) + 1, 2):
         if number % i == 0:
             return False
     return True
 
+
 def all_other_numbers(number):
     result = []
-    a = str(number)
-    for i in range(len(a)):
-        result.append(int(a[-(i+1):len(a)] + a[0:-(i+1)]))
+    array_of_numbers = str(number)
+    for i in range(len(array_of_numbers)):
+        result.append(int(array_of_numbers[-(i+1):len(array_of_numbers)] + array_of_numbers[0:-(i+1)]))
     return result[:-1]
+
 
 def is_circle_simple(number):
     for i in all_other_numbers(number):
-        if is_simple(i) == False:
+        if not is_simple(i):
             return False
     return True
+
+
 n = 1000000
 a = [i for i in range(n + 1)]
 
